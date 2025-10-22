e904fd11a03cc3636a0c2d5bc1e78d2f6a14ac20
Arturo2711 <arturoca971@gmail.com>
2025-10-22 01:39:38 -0600

    First RNN tried, good results it can still become better

--- scripts/window_regressor.py
+++ scripts/window_regressor.py
@@ -13,15 +13,12 @@ from sklearn.model_selection import train_test_split
 
 ### Class to work just with sklearn 
 class Window_Regressor:
-    def __init__(self, time_series_data: pd.Series, window_size: int, horizon: int, test_size :float, model):
+    def __init__(self, time_series_data: pd.Series, window_size: int, horizon: int):
         self.time_series_data = time_series_data
         self.window_size = window_size
         self.horizon = horizon
-        self.model = model
         self.generated_data_set = self.generate_data_set()
-        self.test_size = test_size
-        self.y_pred = None 
-        self.y_true = None 
+    
     
     def generate_data_set(self):
         n = self.time_series_data.shape[0]
@@ -38,18 +35,3 @@ class Window_Regressor:
         
         columns = [f'Predictor_{c}' for c in range(m)] + ['Target']
         return pd.DataFrame(data, columns=columns)
-    
-    def fit_predict(self):
-        data_set = self.generated_data_set
-        if type(data_set):
-            ## Train test split 
-            #print(data_set.columns)
-            X_train, X_test, y_train, y_test = train_test_split(data_set.drop(['Target'], axis=1), data_set['Target'], test_size=self.test_size, shuffle=False)
-            self.model.fit(X_train, y_train)
-            y_pred = self.model.predict(X_test)
-            ## Add y_tue and pred as class atributes 
-            self.y_pred = y_pred
-            self.y_true = y_test
-    
-        else:
-            print('Dataset has not been created yet')
